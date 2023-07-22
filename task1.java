6bb660133b3c9cfb751fba8c529407b59c19e951
Kurkuma731 <125208190+Kurkuma731@users.noreply.github.com>
2023-07-22 16:11:24 +0400

    task1

---
+++ task1.java
@@ -0,0 +1,86 @@
+/* Создать класс Cat
+Создать класс Cat (кот) с пятью инициализаторами:
+- Имя,
+- Имя, вес, возраст
+- Имя, возраст (вес стандартный)
+- вес, цвет, (имя, адрес и возраст неизвестны, это бездомный кот)
+- вес, цвет, адрес ( чужой домашний кот)
+Задача инициализатора – сделать объект валидным. Например, если вес неизвестен, то нужно указать какой-нибудь средний вес. Кот не может ничего не весить. То же касательно возраста. А вот имени может и не быть (null). То же касается адреса: null.
+*/
+public class Cat {
+    private String name;
+    private int weight = 5;
+    private int age = 5;
+    private String color;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void greet() {
+        System.out.println("Мяу! Меня зовут " + name + ". Мне " + age + " года(лет).");
+    }
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int weight, int age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void initialize(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public void initialize(int age, String color, String address) {
+        this.age = age;
+        this.color = color;
+        this.address = address;
+    }
+}
